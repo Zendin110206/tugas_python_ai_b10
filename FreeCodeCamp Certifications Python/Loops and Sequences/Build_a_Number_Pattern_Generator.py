05e56bae4050b61d78720d69fd1f06bc4a35a063
Zaenal Abidin <zaenal.abidin110206@gmail.com>
2026-04-11 00:10:00 +0700

    feat(fcc/loops and sequences): add completed loops and sequences projects

---
+++ FreeCodeCamp Certifications Python/Loops and Sequences/Build_a_Number_Pattern_Generator.py
@@ -0,0 +1,15 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return 'Argument must be an integer value.'
+    if n < 1:
+        return 'Argument must be an integer greater than 0.'
+    
+    number = ''
+    
+    for num in range(1, n+1):
+        number += str(num) + ' '
+    
+    return number.strip()
+
+print(number_pattern(4))
+
